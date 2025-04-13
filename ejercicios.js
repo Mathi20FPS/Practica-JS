aa7ba9f7aae42b0b09a8a462f33f733671888b6e
Matias <matiasperz292@gmail.com>
2025-04-13 14:40:54 -0300

    Ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,19 @@
+//Ejercicio 1
+/*let num1 = 2
+let num2 = 5
+
+let suma = num1 + num2;
+
+console.log("La suma de ambos es: " + suma);*/
+
+let num1 = int(input("Ingresa el primer número: "))
+let num2 = int(input("Ingresa el primer número: "))
+let num3 = int(input("Ingresa el primer número: "))
+let mayor
+let menor
+
+mayor = max(num1, num2, num3)
+menor = min(num1, num2, num3)
+
+console.log("el mayor es: " + mayor);
+console.log("el menor es: " + menor);
